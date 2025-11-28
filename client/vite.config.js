87653e4240e6d39d8913b60a89013d6043f461c7
YadneshUmredkar75 <yadneshUmredkar75@gmail.com>
2025-11-28 16:20:00 +0530

    vercel.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-  
-//   // Important for deployment (fixes MIME & vite.svg issue)
-//   base: './',  
-
-//   server: {
-//     proxy: { 
-//         target: 'https://crm-c1y4.onrender.com',
-//         changeOrigin: true,
-//         secure: false,
-    
-//     },
-//   },
-// })
-// // VITE_API_URL=https://crm-backend.onrender.com
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
