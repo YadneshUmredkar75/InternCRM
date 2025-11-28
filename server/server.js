e463b347d79271c36a420fdfd07d01f19a154f52
YadneshUmredkar75 <yadneshUmredkar75@gmail.com>
2025-11-28 16:47:04 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://crm-seven-jade.vercel.app/", // deployed frontend
+      "https://crm-seven-jade.vercel.app", // deployed frontend
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
