e160df22684894cac771ea082c7251143f842a76
YadneshUmredkar75 <yadneshUmredkar75@gmail.com>
2025-11-28 16:42:32 +0530

    add

--- client/src/pages/AdminLogin.jsx
+++ client/src/pages/AdminLogin.jsx
@@ -23,7 +23,7 @@ const AdminLogin = () => {
 
     try {
       // Vite proxy → /api/admin/login → https://crm-c1y4.onrender.com/api/admin/login
-      const res = await fetch("https://crm-c1y4.onrender.com/api//admin/login", {
+      const res = await fetch("https://crm-c1y4.onrender.com/api/admin/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
